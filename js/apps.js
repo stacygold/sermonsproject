339d933ca9e23ff5d61412cb62404793ebc86e16
stacygold25@gmail.com <stacygold25@gmail.com>
2018-11-08 17:48:11 -0500

    New format of project

--- js/apps.js
+++ js/apps.js
@@ -20,3 +20,53 @@ function newQuote() {
 }
 
 
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageUrl, titleText) {
+    'use strict';
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumbnail) {
+    'use strict';
+    thumbnail.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumbnail);
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict'
+    var thumbnail = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailArray = [].slice.call(thumbnail);
+    return thumbnailArray;
+}
+
+function initializeEvents() {
+    'use strict';
+    var thumbnail = getThumbnailsArray();
+    thumbnail.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
